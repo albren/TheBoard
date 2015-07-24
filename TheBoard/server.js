60cefa9af15698e02256ed86dfb588bb48648a28
Rene Perez <rperez@nearsoft.com>
2015-07-24 11:09:23 -0700

    Setting up static resources in express.

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -8,6 +8,9 @@ var controllers = require('./controllers');
 // Setup the View Engine
 app.set("view engine", "vash");
 
+// Set the public static resources folder
+app.use(express.static(__dirname + "/public"));
+
 // Map the routes
 controllers.init(app);
 
