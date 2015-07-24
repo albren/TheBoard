e9d89b23a6fb0bba7319d7514f18a94b49adba14
Rene Perez <rperez@nearsoft.com>
2015-07-24 11:57:17 -0700

    Installing bower, which is like npm for public resources Installing underscore for the client side. Installing and using bootstrap. Installing grunt and nodemon in order to make development faster by restarting the server automatically when a JS file changes.

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -1,6 +1,4 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-
+﻿// setting up express
 var express = require('express');
 var app = express();
 var controllers = require('./controllers');
@@ -14,6 +12,7 @@ app.use(express.static(__dirname + "/public"));
 // Map the routes
 controllers.init(app);
 
-
-
+// creating server.
+var http = require('http');
+var port = process.env.port || 1337;
 http.createServer(app).listen(port);
